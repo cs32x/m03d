52f6e5fd667ebf6003557254a09a430e5f7a6281
kascrizzi <kascrizzi@gmail.com>
2026-05-07 12:24:48 -0400

    adjust comment

--- find.py
+++ find.py
@@ -1,7 +1,7 @@
 the_string = 'Cosmo the dog'
-the_pattern = 'mo'
+the_pattern = 'me'
 
-# Look for a single-character pattern in a string using a for-loop
+# Look for a pattern in a string using a for-loop
 for i in range(len(the_string)):
     if the_string[i] == the_pattern:
         print("Found at index", i)
